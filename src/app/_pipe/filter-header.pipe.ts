0cf8a5a5d0d04a800142f3626bcd9a969122802d
Dima <vlzkonopatov@gmail.com>
2018-10-30 00:34:32 +0400

    on process

--- src/app/_pipe/filter-header.pipe.ts
+++ src/app/_pipe/filter-header.pipe.ts
@@ -1,19 +1,42 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'filterHeader'
 })
 export class FilterHeaderPipe implements PipeTransform {
   headerItem: Array<any>;
+  element: any;
 
   transform(value: any, args?: any): any {
     this.headerItem = [];
-    console.log(value);
-    const obj = Object.keys(value).map((k) => k);
-    console.log(obj);
+    this.element = value;
+    // console.log(this.element);
+
+    let item = [];
+    item.push(this.objToArray(this.element));
+
+    console.log(this.headerItem);
 
 
     return this.headerItem;
+  }
+
+  public objToArray(object) {
+
+    console.log(object);
+
+    let obj = Object.keys(object).map((k) => k);
+
+    this.headerItem.push(obj.map((key) => {
+        console.log(key);
+
+        if (typeof object[key] === "object") {
+          this.objToArray(object[key])
+        }
+
+        return key
+      })
+    );
   }
 
 }
